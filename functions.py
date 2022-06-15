c86f0622bb3b7657b69faa6f41ce628946a6ea38
imac1 <irina.macovei@yahoo.fr>
2022-06-15 14:47:51 +0300

    final

--- functions.py
+++ functions.py
@@ -1,20 +1,17 @@
+from collections import Counter
+from datetime import datetime
+
+
+
 def parse_user_data(line):
     """
     >>> parse_user_data('John Doe john.doe@example.com')
     ('John', 'Doe', 'john.doe', 'example.com')
     """
 
-    parts = line.split(' ')
-    first_name = parts[0]
-    last_name = parts[1]
-    email = parts[2]
-
-    email_parts = email.split('@')
-    user = email_parts[0]
-    host = email_parts[1]
-
-    response = (first_name, last_name, user, host)
-    return response
+    first_name, last_name, email = line.split(' ')
+    user, host = email.split('@')
+    return first_name, last_name, user, host
 
 
 def compare_lists(dir_a, dir_b):
@@ -24,44 +21,20 @@ def compare_lists(dir_a, dir_b):
     >>> compare_lists(dir_a, dir_b)
     {'removed': ['hello.py'], 'added': ['hello2.py', 'install.txt']}
     """
-
-    removed = []
-    for filename in dir_a:
-        if filename not in dir_b:
-            removed.append(filename)
-
-    added = []
-    for filename in dir_b:
-        if filename not in dir_a:
-            added.append(filename)
-
-    return {'removed': sorted(removed), 'added': sorted(added)}
+    a, b = set(dir_a), set(dir_b)
+    return {'removed': sorted(a - b), 'added': sorted([x for x in b if x not in a])}
 
 
-def print_log(message, process_id, timestamp, level=2):
+def print_log(message, process_id, timestamp, level=5):
     """
     >>> from datetime import datetime
     >>> print_log('System started!', 1532, datetime(2019, 1, 2, 10, 30, 55).isoformat(' '))
     2019-01-02 10:30:55 [1532] [INFO] System started!
     """
-
-    line = timestamp
-    line += ' [' + str(process_id) + ']'
-    if level == 0:
-        loglevel = 'TRACE'
-    elif level == 1:
-        loglevel = 'DEBUG'
-    elif level == 2:
-        loglevel = 'INFO'
-    elif level == 3:
-        loglevel = 'WARN'
-    elif level == 4:
-        loglevel = 'ERROR'
-    else:
-        loglevel = 'None'
-    line += ' [' + loglevel + ']'
-    line += ' ' + message
-    print(line)
+    if level > 4:
+        level = 5
+    level2 = {0: 'TRACE', 1: 'DEBUG', 2: 'INFO', 3: 'WARN', 4: 'ERROR', 5: 'None'}
+    return print(f' {timestamp} {[process_id]}  {message} {level2[level]}')
 
 
 def biggest_rectangle(rectangles):
@@ -72,12 +45,7 @@ def biggest_rectangle(rectangles):
     >>> biggest_rectangle([(2, 4), (3, 3), (4, 2)])
     (3, 3)
     """
-
-    best = rectangles[0]
-    for r in rectangles:
-        if r[0] * r[1] > best[0] * best[1]:
-            best = r
-    return best
+    return max(rectangles, key=lambda item: item[1]*item[0])
 
 
 def find_in_file(pattern, filename):
@@ -97,52 +65,48 @@ def find_in_file(pattern, filename):
     123 Quoth the Raven, "Nevermore."
     132 Quoth the Raven, "Nevermore."
     140 Shall be lifted- nevermore!
+
+    max 4 lines
     """
 
-    f = open(filename)
-    try:
-        line_num = 0
-        for line in f:
-            char_num = 0
-            while line[char_num] == ' ':
-                char_num = char_num + 1
-            line = line[char_num: -1]
-            if pattern.lower() in line.lower():
-                spaces = ''
-                if line_num < 10:
-                    spaces = '  '
-                elif line_num < 100:
-                    spaces = ' '
-                print(spaces + str(line_num) + ' ' + line)
-            line_num += 1
-    finally:
+    with open(filename, 'r') as f:
+        # lines = Counter([line for line in f if pattern in line.lower()])
+        for count, line in enumerate(f):
+            if pattern in line.lower():
+                count += 1
+                print(f'{count-1} {line}')
         f.close()
+        return print(f'{count-1} {line}')
+        # return print(lines)
+        
 
-
-def read_long_words(filename, min_length=0):
+def read_long_words(filename, min_length):
     """
     >>> words = read_long_words('raven.txt', 5)
     >>> words[:6]
     ['midnight', 'dreary', 'pondered', 'quaint', 'curious', 'volume']
     """
 
-    f = open(filename)
-    try:
-        content = f.read()
-    finally:
-        f.close()
+    return [word.strip("r'[.,\"!-]'") for word in (open(filename).read()).split() if len(word) > min_length]
 
-    # Remove punctuation
-    no_punct = []
-    for ch in content:
-        if ch not in r'[.,"!-]':
-            no_punct.append(ch)
-    content = ''.join(no_punct)
-    result = []
-    for word in content.split():
-        if len(word) > min_length:
-            result.append(word.lower())
-    return result
+
+    # with open(filename, 'r') as f:
+    #     try:
+    #         content = f.read()
+    #     finally:
+    #         f.close()
+
+    #     # Remove punctuation
+    #     no_punct = []
+    #     for ch in content:
+    #         if ch not in r'[.,"!-]':
+    #             no_punct.append(ch)
+    #     content = ''.join(no_punct)
+    #     result = []
+    #     for word in content.split():
+    #         if len(word) > min_length:
+    #             result.append(word.lower())
+    #     return result
 
 
 def top_words(words, n=10):
@@ -154,18 +118,25 @@ def top_words(words, n=10):
     [('chamber', 11), ('nevermore', 10), ('lenore', 8), ('nothing', 7), ('tapping', 5)]
     """
 
-    word_counts = {}
-    for word in words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
+    # word_counts = {}
+    # for word in words:
+    #     if word in word_counts:
+    #         word_counts[word] += 1
+    #     else:
+    #         word_counts[word] = 1
 
-    result = []
-    for word, count in word_counts.items():
-        # Append (count, word) so that we sort by count.
-        result.append((count, word))
+    # result = []
+    # for word, count in word_counts.items():
+    #     # Append (count, word) so that we sort by count.
+    #     result.append((count, word))
 
-    result.sort(reverse=True)
-    result = result[:n]
-    return [(count, word) for (word, count) in result]
+    # result.sort(reverse=True)
+    # result = result[:n]
+    # return [(count, word) for (word, count) in result]
+    words = Counter(words).items()
+    return sorted(words, key=lambda item: item[1], reverse=True)[:n]
+
+
+if __name__ == '__main__':
+    words = read_long_words('raven.txt', 5)
+    print(top_words(words, n=10))
